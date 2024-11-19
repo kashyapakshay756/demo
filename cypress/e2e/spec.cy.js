cc6ce221554b73a5a22569577a20bf2322c9dfc3
AkshayVhits <158267550+AkshayVhits@users.noreply.github.com>
2024-11-19 18:39:57 +0530

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 describe('Template Spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000'); // Ensure you include the protocol (http:// or https://)
+    cy.visit('http://localhost:8080'); // Ensure you include the protocol (http:// or https://)
   });
 
   it('verifies email with an invalid input', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('test');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
     cy.get('#filled-hidden-label-normal-helper-text')
@@ -13,7 +13,7 @@ describe('Template Spec', () => {
   });
   
   it('verifies email with an invalid input syntex', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('test.com');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
     cy.get('#filled-hidden-label-normal-helper-text')
@@ -22,7 +22,7 @@ describe('Template Spec', () => {
   });
 
   it('verifies email with an invalid input by number', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('456781368456');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
     cy.get('#filled-hidden-label-normal-helper-text')
@@ -31,20 +31,20 @@ describe('Template Spec', () => {
   });
 
   it('verifies email with an invalid input by symbol', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('&*%^*&%&^&*');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
   });
 
   it('verifies email with an Blank input', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('  ');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
 
   });
   
   it('verifies email with an valid input', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('admin@zeebra.com');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
     cy.get('#outlined-adornment-password-helper-text')
@@ -54,7 +54,7 @@ describe('Template Spec', () => {
 
   
   it('verifies password with an Blank input', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('admin@zeebra.com');
     cy.get('#outlined-adornment-password').type('  ');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
@@ -63,7 +63,7 @@ describe('Template Spec', () => {
 
   
   it('verifies password with an invalid input', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('admin@zeebra.com');
     cy.get('#outlined-adornment-password').type('sdjghakjshg');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
@@ -71,7 +71,7 @@ describe('Template Spec', () => {
   
     
   it('verifies password with an valid input', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.get('#filled-hidden-label-normal').type('admin@zeebra.com');
     cy.get('#outlined-adornment-password').type('Admin@123');
     cy.get('.orange-btn > .MuiButtonBase-root').click();
